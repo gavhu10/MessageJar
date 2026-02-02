684c9ccf2c8bddfc75f8ef9dc938790ef3a410ca
gavhu10 <186771458+gavhu10@users.noreply.github.com>
2026-02-02 11:16:28 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,10 @@
 import flask as f
-import user
 import auth
 
 import backend as cb
 from backend import NotAllowedError, AuthError
 from auth import RegistrationError
 
-status_user = "Message Jar"
 
 api = f.Blueprint("api", __name__, url_prefix="/api")
 
@@ -19,84 +17,53 @@ def get_kv(request, keys, optional=[]):
 
         if key in optional:
             continue
-        if key not in r or not r[key]:
+        if r[key] is None:
             raise ValueError(f"Missing argument: {key}")
 
     return r
 
 
-@api.route("/api-get", methods=["GET", "POST"])
+@api.route("/get", methods=["GET", "POST"])
 def api_get():
 
-    args = {
-        "username": "",
-        "password": "",
-        "latest": "",
-        "room": "",
-    }
-
-    optional = ["latest"]
-
-    for key, value in args.items():
-        if f.request.method == "POST":
-            args[key] = f.request.form.get(key)
-        elif f.request.method == "GET":
-            args[key] = f.request.args.get(key)
+    try:
+        args = get_kv(f.request, ["token", "latest", "room"], ["latest"])
+    except ValueError:
+        f.abort(400)
 
     try:
         latest = int(args["latest"])
     except (ValueError, TypeError):
         latest = 0
 
-    for key, value in args.items():
-
-        if key in optional:
-            continue
-
-        if not value:
-            error = f"Missing argument: {key}"
-            f.abort(400)
-
     try:
-        auth.check_user(args["username"], args["password"])
+        username = auth.check_valid_token(args["token"])
     except AuthError:
         f.abort(403)
     else:
-
+        if username not in cb.get_room_members(args["room"]):
+            f.abort(403)
         return f.jsonify(cb.get_messages(args["room"], latest))
 
 
-@api.route("/api-send", methods=["POST", "GET"])
+@api.route("/send", methods=["POST", "GET"])
 def api_send():
 
-    args = {
-        "username": "",
-        "password": "",
-        "message": "",
-        "room": "",
-    }
-
-    for key, value in args.items():
-        if f.request.method == "POST":
-            args[key] = f.request.form.get(key)
-        elif f.request.method == "GET":
-            args[key] = f.request.args.get(key)
-
-    for key, value in args.items():
-        if not value:
-            error = f"Missing argument: {key}"
-            f.abort(400)
+    try:
+        args = get_kv(f.request, ["token", "message", "room"])
+    except ValueError:
+        f.abort(400)
 
     try:
-        auth.check_user(args["username"], args["password"])
+        username = auth.check_valid_token(args["token"])
     except AuthError:
         f.abort(403)
 
-    rooms = cb.get_rooms(args["username"])
+    rooms = cb.get_rooms(username)
     if not args["room"] in rooms:
         f.abort(403)
 
-    cb.add_message(args["username"], args["message"], args["room"])
+    cb.add_message(username, args["message"], args["room"])
     return "OK"
 
 
@@ -116,13 +83,13 @@ def manage_rooms():
         case "list_rooms":
             rooms = cb.get_rooms(username)
             return f.jsonify(rooms)
-        
+
         case "create_room":
             try:
                 cb.create_room(args["room"], username)
             except NotAllowedError:
                 f.abort(403)
-            return "OK"
+            return f.jsonify({"status": "ok"})
         case _:
             f.abort(400)
 
@@ -141,24 +108,21 @@ def manage_user():
             f.abort(403)
 
     match args["action"]:
-        case "get_username":
-            try:
-                return auth.check_valid_token(args["token"])
-            except AuthError:
-                f.abort(403)
         case "new_user":
             try:
                 auth.register_user(args["username"], args["password"])
             except RegistrationError:
                 f.abort(403)
-            return "OK"
+            return f.jsonify({"status": "ok"})
+        case "list_tokens":
+            return f.jsonify(cb.list_tokens(args["username"]))
         case "verify_user":
             try:
                 auth.check_user(args["username"], args["password"])
             except AuthError:
                 f.abort(403)
 
-            return "OK"
+            return f.jsonify({"status": "ok"})
         case "generate_token":
             if args["name"]:
                 return f.jsonify(
@@ -182,21 +146,10 @@ def manage_token():
         f.abort(403)
 
     match args["action"]:
-        case "list_tokens":
-            return f.jsonify(cb.list_tokens(username))
+        case "get_username":
+            return f.jsonify({"username": username})
         case "revoke_token":
             auth.revoke_api_token(args["token"])
-            return "OK"
-        case "get_token_by_name":
-            if args["name"]:
-                tokens = cb.list_tokens(username)
-                token = [i for i in tokens if i["tokenname"] == args["name"]][0][
-                    "token"
-                ]
-                if token:
-                    return f.jsonify({"token": token})
-                else:
-                    f.abort(404)
-            f.abort(400)
+            return f.jsonify({"status": "ok"})
         case _:
             f.abort(400)
