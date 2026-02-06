c0ec0734495d49e84e673423737f3da384c1ad81
gavhu10 <186771458+gavhu10@users.noreply.github.com>
2026-02-05 20:51:11 -0500

    Add user settings page

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import flask as f
+import backend as cb
 import auth
 from backend import AuthError
 
@@ -6,15 +7,15 @@ user = f.Blueprint("user", __name__, url_prefix="/user")
 
 
 @user.route("/")
-def user_index():
-    return f.redirect(f.url_for("user.pass_change"))
+@auth.login_required
+def index():
+    return f.render_template("user/main.html")
 
 
-@user.route("/passchange", methods=("GET", "POST"))
+@user.route("/passchange", methods=["GET", "POST"])
+@auth.login_required
 def pass_change():
     """Change the password for a logged-in user."""
-    if "username" not in f.session:
-        return f.redirect(f.url_for("auth.login"))
 
     if f.request.method == "POST":
         old_password = f.request.form["old_password"]
@@ -37,3 +38,54 @@ def pass_change():
         f.flash(error)
 
     return f.render_template("user/pass_change.html")
+
+
+@user.route("/rmtoken")
+@auth.login_required
+def rmtoken():
+    message = None
+
+    token = f.request.args.get("token")
+
+    if token is None:
+        message = "Token is required!"
+    else:
+        try:
+            if auth.check_valid_token(token) != f.g.user["username"]:
+                raise AuthError
+            else:
+                auth.revoke_api_token(token)
+        except AuthError:
+            message = "Invalid token!"
+
+    if message is None:
+        message = "Token revoked."
+
+    f.flash(message)
+    return f.redirect(f.url_for("user.tokens"))
+
+
+@user.route("/tokens", methods=["GET", "POST"])
+@auth.login_required
+def tokens():
+    if f.request.method == "GET":
+        return f.render_template(
+            "user/token.html", token_list=cb.list_tokens(f.g.user["username"])
+        )
+
+    message = None
+
+    token_name = f.request.form.get("token_name")
+
+    if not token_name:
+        message = "Token name is required!"
+
+    if message is None:
+        try:
+            auth.generate_api_token(f.g.user["username"], token_name)
+            message = "Token sucsessfully created."
+        except auth.NotAllowedError as e:
+            message = e.message
+
+    f.flash(message)
+    return f.redirect(f.url_for("user.tokens"))
